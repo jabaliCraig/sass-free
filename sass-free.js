ce075b0f8ceaf4fb81605380b73b8ddcb22a7a3b
Craig <71907929+jabaliCraig@users.noreply.github.com>
2023-07-19 18:49:53 -0600

    Create sass-free.js

---
+++ sass-free.js
@@ -0,0 +1,10 @@
+const answerKey = {
+	pickaxeOfCortez: "https://i.kym-cdn.com/entries/icons/facebook/000/018/682/obi-wan.jpg",
+  mysticalDiamonds: "https://i.kym-cdn.com/entries/icons/facebook/000/018/682/obi-wan.jpg",
+	notYourSass: "https://i.kym-cdn.com/entries/icons/facebook/000/018/682/obi-wan.jpg",
+	glueLessons: "https://cdn.vox-cdn.com/thumbor/E7L-LRW6yxGSIOTGHITfZK_GzBE=/0x0:1920x816/1200x0/filters:focal(0x0:1920x816):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/11448849/FIRST_INSET.jpg",
+	permissionToLightFireworks: "https://cdn.vox-cdn.com/thumbor/E7L-LRW6yxGSIOTGHITfZK_GzBE=/0x0:1920x816/1200x0/filters:focal(0x0:1920x816):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/11448849/FIRST_INSET.jpg",
+	mapOfNevada: "http://assets2.ignimgs.com/2014/04/09/luke-tatooine-1280jpg-0a6069_1280w.jpg",
+	grilledMeat: "http://vignette4.wikia.nocookie.net/villains/images/c/c6/Greedo.jpg/revision/latest?cb=20110221183905",
+	pets: "http://vignette4.wikia.nocookie.net/villains/images/c/c6/Greedo.jpg/revision/latest?cb=20110221183905"
+}
